81f61df2fd79a56d48e148f6864b2a26ac2c3b52
Amine MEKKI <amine.mekki@mines-ales.org>
2023-11-04 19:54:40 +0100

    updating algorithms calling and execution

--- solution.py
+++ solution.py
@@ -1,49 +1,72 @@
 import sys
-from src.algorithms.greedy import heuristic
+from src.algorithms.greedy import greedy_tsp
 from src.algorithms.SA import simulated_annealing
 from src.algorithms.brute_force import brute_force
 from src.utils.helpers import prepare_data
-import os
-import random 
-def main():
-    if len(sys.argv) != 5:
-        print("Usage: python script.py <instance_file> <algorithm_type> <cutoff> <rand_seed>")
-        sys.exit(1)
-    
-    instance_file, algorithm_type, cutoff_time, rand_seed  = sys.argv[1:5]
-    cutoff_time, rand_seed = float(cutoff_time), int(rand_seed)
-    dist_matrix, num_locs = prepare_data("instances/" + instance_file)
-    
-    if algorithm_type == "greedy":
-        result = heuristic(rand_seed, dist_matrix, num_locs, cutoff_time)  
-        solution_path = "solutions/greedy/" 
-    
-    elif algorithm_type == "SA":
-        result = simulated_annealing(rand_seed, dist_matrix, cutoff_time, 1000, 0.01)
-        solution_path = "solutions/SA/"
-        
-    elif algorithm_type == "brute_force":
-        result = brute_force(dist_matrix)
-        solution_path = "solutions/brute_force/"
-      
-        
-    instance_name = instance_file.split('.')[0]
-    solution_file = f"{solution_path}/{instance_name}_{algorithm_type}_{cutoff_time}_{rand_seed}.sol"
-    trace_file = f"{solution_path}/{instance_name}_{algorithm_type}_{cutoff_time}_{rand_seed}.trace"
-    path_only_file = f"{solution_path}/{instance_name}_{algorithm_type}_{cutoff_time}_{rand_seed}_path_only.txt"
-    
-    with open(solution_file, 'w') as handle:
-        handle.write(f"{result[0]}\n")
-        handle.writelines(f"{a} {b} {c}\n" for a, b, c in result[1:-1])
-    
-    with open(trace_file, 'w') as handle:
-        handle.write(f"{result[-1]}, {result[0]}")
-        
-    with open(path_only_file, 'w') as handle:
-        handle.write(" ".join(str(a) for a, _, _ in result[1:-1]))
+from src.algorithms.DP import TspDynamicProgrammingRecursive
+from src.algorithms.ant_colony import ant_colony_optimization
+from src.algorithms.GA import TravelingSalesmanGA
 
-    
+
+def parse_arguments():
+    if len(sys.argv) != 5:
+        print(
+            "Usage: python script.py <instance_file> <algorithm_type> <cutoff> <rand_seed>")
+        sys.exit(1)
+
+    return sys.argv[1:5]
+
+
+def choose_algorithm(algorithm_type, instance_name, rand_seed, dist_matrix, num_locs, cutoff_time):
+
+    if algorithm_type == "greedy":
+        solution_path = "solutions/greedy/"
+        return greedy_tsp(rand_seed, dist_matrix, num_locs, cutoff_time, solution_path, instance_name), solution_path
+
+    elif algorithm_type == "SA":
+        solution_path = "solutions/SA/"
+        result = simulated_annealing(
+            dist_matrix, num_locs, cutoff_time, solution_path, instance_name, rand_seed)
+
+        return result, "solutions/SA/"
+
+    elif algorithm_type == "brute_force":
+        return brute_force(dist_matrix), "solutions/brute_force/"
+
+    elif algorithm_type == "DP":
+        solution_path = "solutions/DP/"
+        solver = TspDynamicProgrammingRecursive(
+            dist_matrix, instance_name, cutoff_time, rand_seed, solution_path)
+        return solver.get_metrics()
+
+    elif algorithm_type == "ACO":
+        solution_path = "solutions/ACO/"
+        return ant_colony_optimization(dist_matrix, instance_name, cutoff_time, rand_seed, solution_path), solution_path
+
+    elif algorithm_type == "GA":
+        solution_path = "solutions/GeneticAlgorithm/"
+        solver = TravelingSalesmanGA(
+            dist_matrix, instance_name, cutoff_time, rand_seed, solution_path)
+        return solver.run(time_limit=cutoff_time), solution_path
+
+    else:
+        print("Invalid algorithm type")
+        sys.exit(1)
+
+
+def main():
+
+    instance_file, algorithm_type, cutoff_time, rand_seed = parse_arguments()
+    cutoff_time, rand_seed = float(cutoff_time), int(rand_seed)
+    instance_name = instance_file.split('.')[0]
+
+    dist_matrix, num_locs = prepare_data("instances/" + instance_file)
+
+    result, solution_path = choose_algorithm(
+        algorithm_type, instance_file, rand_seed, dist_matrix, num_locs, cutoff_time)
+
+    print("SA result  : ", result)
+
+
 if __name__ == '__main__':
     main()
-   
-
