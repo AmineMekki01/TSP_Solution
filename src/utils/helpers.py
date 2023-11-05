966ef7a9a9a95321910d34b307d3cda164cea831
Amine MEKKI <amine.mekki@mines-ales.org>
2023-11-05 02:02:25 +0100

    cleaning code

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,6 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy.spatial.distance import squareform, pdist
 
 
 def set_diag_to_inf(matrix):
@@ -19,23 +17,3 @@ def prepare_data(file_path):
     dist_matrix = np.sqrt(((coords[:, np.newaxis, :] - coords)**2).sum(axis=2))
     np.fill_diagonal(dist_matrix, np.inf)
     return dist_matrix, len(coords)
-
-
-
-def plot_graph(df, nodes):
-    plt.scatter(df['X'], df['Y'], c='blue')
-
-    for idx, row in df.iterrows():
-        plt.text(row['X'], row['Y'], str(idx))
-
-    for i in range(len(nodes) - 1):
-        point1 = df.loc[nodes[i]]
-        point2 = df.loc[nodes[i + 1]]
-        plt.plot([point1['X'], point2['X']], [point1['Y'], point2['Y']], 'k-')
-
-    point1 = df.loc[nodes[-1]]
-    point2 = df.loc[nodes[0]]
-    plt.plot([point1['X'], point2['X']], [point1['Y'], point2['Y']], 'k-')
-
-    plt.show()
-    
